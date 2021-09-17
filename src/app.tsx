b816057e05debe8ad77d23ba89bcf0ac1592ead9
Gabriel <dev.gabrielramos@gmail.com>
2021-09-17 14:43:54 -0300

    Render main content component

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,13 @@
 import { Sidebar } from 'components/sidebar'
+import { MainContent } from 'components/content-area'
+
+import * as S from './styles/app-styles'
 
 export function App () {
   return (
     <S.Container>
       <Sidebar />
-    </>
+      <MainContent />
+    </S.Container>
   )
 }
