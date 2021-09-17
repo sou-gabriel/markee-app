e6d96757bc69636b55f14fa654f10f2e9a4fe02d
Gabriel <dev.gabrielramos@gmail.com>
2021-09-17 14:42:35 -0300

    Create text output component

---
+++ src/components/text-output/style.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components/macro'
+
+export const Output = styled.output`
+  font-size: 1.8rem;
+  line-height: 2.344rem;
+  color: ${props => props.theme.colors.black};
+`
---
+++ src/components/text-output/text-output.tsx
@@ -0,0 +1,5 @@
+import * as S from './style'
+
+export function TextOutput () {
+  return <S.Output>O resultado será exibido aqui</S.Output>
+}
