a308dfa41914b5752c9a2fc7bffc1740a1034569
Gabriel <dev.gabrielramos@gmail.com>
2021-09-20 13:44:14 -0300

    Fix text area font style

--- src/components/text-area/styles.ts
+++ src/components/text-area/styles.ts
@@ -3,9 +3,9 @@ import styled from 'styled-components/macro'
 export const TextArea = styled.textarea`
   width: 100%;
   height: 100%;
-  font-family: 'Inconsolate', sans-serif;
+  font-family: 'Inconsolata', sans-serif;
   font-weight: 500;
-  font-size: 1.6rem;
+  font-size: 1.8rem;
   line-height: 1.888rem;
   color: ${props => props.theme.colors.black};
   resize: none;
