a2b1d3466e16f9a6f2df688aa3e67f9448fef5e7
Gabriel <dev.gabrielramos@gmail.com>
2021-09-21 00:19:49 -0300

    Update the file list component

--- src/components/file-list/file-list.tsx
+++ src/components/file-list/file-list.tsx
@@ -1,17 +1,28 @@
-import { FileItem } from 'components/file-item'
+import { Dispatch, SetStateAction, RefObject } from 'react'
 
-import * as S from './styles'
+import { FileItem } from 'components/file-item'
 
 import { FileType } from 'resources/types/file'
 
+import * as S from './styles'
+
 type FileListProps = {
   files: FileType[]
+  inputRef: RefObject<HTMLInputElement>
+  setFiles: Dispatch<SetStateAction<FileType[]>>
 }
 
-export function FileList (props: FileListProps) {
+export function FileList ({ files, inputRef, setFiles }: FileListProps) {
   return (
     <S.FileList>
-      {props.files.map(file => <FileItem key={file.id} file={file} />)}
+      {files.map(file => (
+        <FileItem
+          inputRef={inputRef}
+          key={file.id}
+          file={file}
+          setFiles={setFiles}
+        />
+      ))}
     </S.FileList>
   )
 }
