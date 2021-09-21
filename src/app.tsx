2b312553f5eb7cf4276bd29567cc66fd742a3ce8
Gabriel <dev.gabrielramos@gmail.com>
2021-09-21 00:02:42 -0300

    Add functionality to update status and update prop drilling

--- src/app.tsx
+++ src/app.tsx
@@ -9,6 +9,40 @@ import { StatusType } from 'resources/types/status'
 
 export function App () {
   const inputRef = useRef<HTMLInputElement>(null)
+  const [files, setFiles] = useState<FileType[]>([])
+
+  useEffect(() => {
+    const fileActive = files.find(file => file.active)
+    const isTheFileBeingEdited = fileActive && fileActive.status === 'editing'
+
+    let externalTimerId: ReturnType<typeof setTimeout>
+
+    const createStatusUpdateFunction = (status: StatusType) => {
+      return (files: FileType[]) => files.map<FileType>(file => {
+        return file.active ? { ...file, status } : file
+      })
+    }
+
+    if (isTheFileBeingEdited) {
+      const changeFileStatus = () => {
+        externalTimerId = setTimeout(() => {
+          const updateStatusForSaving = createStatusUpdateFunction('saving')
+          setFiles(updateStatusForSaving)
+
+          const internalTimerId = setTimeout(() => {
+            const updateStatusForSaved = createStatusUpdateFunction('saved')
+            setFiles(updateStatusForSaved)
+
+            clearTimeout(internalTimerId)
+          }, 300)
+        }, 300)
+      }
+
+      changeFileStatus()
+    }
+
+    return () => clearTimeout(externalTimerId)
+  }, [files])
 
   return (
     <S.Container>
