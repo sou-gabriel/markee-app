109bb04cfe11115399a556e1cbef25e07efeb096
Gabriel <dev.gabrielramos@gmail.com>
2021-09-21 00:18:47 -0300

    Create switch and delete file functionality

--- src/components/file-item/file-item.tsx
+++ src/components/file-item/file-item.tsx
@@ -1,3 +1,5 @@
+import { MouseEvent, Dispatch, SetStateAction, RefObject } from 'react'
+
 import { StatusIcon } from 'components/status-icon'
 
 import { ReactComponent as FileIcon } from 'resources/assets/images/file-text.svg'
@@ -8,17 +10,37 @@ import * as S from './styles'
 import { FileType } from 'resources/types/file'
 
 type FileItemProps = {
+  inputRef: RefObject<HTMLInputElement>
   file: FileType
+  setFiles: Dispatch<SetStateAction<FileType[]>>
 }
 
-export function FileItem (props: FileItemProps) {
+export function FileItem ({ inputRef, file: clickedFile, setFiles }: FileItemProps) {
+  const handleFileChange = (event: MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault()
+    inputRef.current?.focus()
+
+    setFiles(prevFiles => prevFiles.map<FileType>(file => file.id === clickedFile.id
+      ? { ...file, active: true }
+      : { ...file, active: false },
+    ))
+  }
+
+  const handleDeleteFile = () => {
+    inputRef.current?.focus()
+
+    setFiles(prevFiles =>
+      prevFiles.filter(prevFile => prevFile.id !== clickedFile.id),
+    )
+  }
+
   return (
-    <S.FileItem active={props.file.active} status={props.file.status}>
+    <S.FileItem active={clickedFile.active} status={clickedFile.status}>
       <FileIcon />
-      <S.Link href='/'>{props.file.name}</S.Link>
-      {props.file.active
-        ? <StatusIcon status={props.file.status} />
-        : <S.ButtonDeleteFile><DeleteIcon /></S.ButtonDeleteFile>}
+      <S.Link href='/' onClick={handleFileChange}>{clickedFile.name}</S.Link>
+      {clickedFile.active
+        ? <StatusIcon status={clickedFile.status} />
+        : <S.ButtonDeleteFile onClick={handleDeleteFile}><DeleteIcon /></S.ButtonDeleteFile>}
     </S.FileItem>
   )
 }
