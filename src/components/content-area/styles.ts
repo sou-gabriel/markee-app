c07eb8547dc4d3c471200ddbf1cf07efdd22ff7d
Gabriel <dev.gabrielramos@gmail.com>
2021-09-23 22:54:39 -0300

    Fix long text bug

--- src/components/content-area/styles.ts
+++ src/components/content-area/styles.ts
@@ -75,6 +75,7 @@ export const Output = styled.output`
   font-size: 1.8rem;
   line-height: 2.344rem;
   color: ${props => props.theme.colors.black};
+  word-break: break-all;
 
   > * {
     margin-top: 0;
